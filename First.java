5f014e13e3b7c30c3d554e5fcaf6ae1ec746f8d4
SeungJun <sj021210@naver.com>
2024-12-22 19:33:31 +0900

    version 0.1 메뉴바 배치 및 ui 배정

--- First.java
+++ First.java
@@ -14,6 +14,8 @@ public class First extends JFrame {
      * @changelog
      * <ul>
      *     <li>2024-12-09 : 실시간 년,월,일,시,분,초 출력할 수 있도록 생성 (SeungJun)</li>
+     *     <li>2024-12-21 : 메뉴바 생성 및 추가(SeungJun)</li>
+     *     <li>2024-12-22 :</li>
      *
      * </ul>
      * @see<a href = "https://hianna.tistory.com/607"></a>
@@ -23,6 +25,18 @@ public class First extends JFrame {
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        //메뉴바 만들기
+        JMenuBar menuBar = new JMenuBar();
+        //메뉴바에 공지사항 추가
+        JMenu notice = new JMenu("공지사항");
+        menuBar.add(notice);
+        //메뉴바에 좌석 예약 추가
+        JMenu reserve = new JMenu("좌석 예약");
+        menuBar.add(reserve);
+
+        setJMenuBar(menuBar);
+
+
 
         // 패널 생성
         JPanel panel = new JPanel();
